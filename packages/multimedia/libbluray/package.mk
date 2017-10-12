9de98d9b6136c790a2ac341f2bf8154ec4a64aa1
afl1 <afl2001@gmail.com>
2017-10-12 20:34:09 +0200

    libbluray: ver 1.0.1

--- packages/multimedia/libbluray/package.mk
+++ packages/multimedia/libbluray/package.mk
@@ -17,14 +17,13 @@
 ################################################################################
 
 PKG_NAME="libbluray"
-PKG_VERSION="0.9.3"
-PKG_REV="1"
+PKG_VERSION="1.0.1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.videolan.org/developers/libbluray.html"
 PKG_URL="https://ftp.videolan.org/pub/videolan/libbluray/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain fontconfig freetype libxml2"
-PKG_SECTION="multimedia"
+KG_SECTION="multimedia"
 PKG_SHORTDESC="libbluray: A Blu-Ray Discs playback library"
 PKG_LONGDESC="libbluray is an open-source library designed for Blu-Ray Discs playback for media players, like VLC or MPlayer."
 
@@ -43,8 +42,7 @@ PKG_CONFIGURE_OPTS_TARGET="--disable-werror \
                            --disable-extra-warnings \
                            --disable-optimizations \
                            --disable-examples \
-                           --disable-bdjava \
-                           --enable-udf \
+                           --disable-bdjava-jar \
                            --disable-doxygen-doc \
                            --disable-doxygen-dot \
                            --disable-doxygen-man \
