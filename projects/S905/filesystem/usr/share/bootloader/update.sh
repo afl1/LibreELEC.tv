63bc3c0d37b18df9c66cbe05fcd24aa6078ac8e4
afl1 <afl2001@gmail.com>
2017-11-06 00:21:44 +0100

    project/S905: K-Series patches

--- projects/S905/filesystem/usr/share/bootloader/update.sh
+++ projects/S905/filesystem/usr/share/bootloader/update.sh
@@ -23,22 +23,14 @@
 [ -z "$UPDATE_DIR" ] && UPDATE_DIR="/storage/.update"
 UPDATE_DTB_IMG="$UPDATE_DIR/dtb.img"
 UPDATE_DTB=`ls -1 "$UPDATE_DIR"/*.dtb 2>/dev/null | head -n 1`
-[ -z "$BOOT_PART" ] && BOOT_PART=$(df "$BOOT_ROOT" | tail -1 | awk {' print $1 '})
-if [ -z "$BOOT_DISK" ]; then
-  case $BOOT_PART in
-    /dev/sd[a-z][0-9]*)
-      BOOT_DISK=$(echo $BOOT_PART | sed -e "s,[0-9]*,,g")
-      ;;
-    /dev/mmcblk*)
-      BOOT_DISK=$(echo $BOOT_PART | sed -e "s,p[0-9]*,,g")
-      ;;
-  esac
-fi
+
+# Indicate that we are not modifying bootloader
+echo "Bootloader has NOT been modified."
+echo "Updating device tree and partition labels..."
 
 for arg in $(cat /proc/cmdline); do
   case $arg in
     boot=*)
-      echo "*** updating BOOT partition label ..."
       boot="${arg#*=}"
       case $boot in
         /dev/mmc*)
@@ -49,21 +41,18 @@ for arg in $(cat /proc/cmdline); do
           ;;
       esac
 
-      LE_DT_ID=$(cat /proc/device-tree/le-dt-id)
       if [ -f "$UPDATE_DTB_IMG" ] ; then
         UPDATE_DTB_SOURCE="$UPDATE_DTB_IMG"
       elif [ -f "$UPDATE_DTB" ] ; then
         UPDATE_DTB_SOURCE="$UPDATE_DTB"
-      elif [ -n "$LE_DT_ID" -a -f "$SYSTEM_ROOT/usr/share/bootloader/$LE_DT_ID.dtb" ] ; then
-        UPDATE_DTB_SOURCE="$SYSTEM_ROOT/usr/share/bootloader/$LE_DT_ID.dtb"
       fi
 
       if [ -f "$UPDATE_DTB_SOURCE" ] ; then
-        echo "*** updating device tree from $UPDATE_DTB_SOURCE ..."
+        echo "Updating device tree from $UPDATE_DTB_SOURCE..."
         case $boot in
           /dev/system)
-            dd if=/dev/zero of=/dev/dtb bs=256k count=1 status=none
-            dd if=$UPDATE_DTB_SOURCE of=/dev/dtb bs=256k status=none
+            dd if=/dev/zero of=/dev/dtb bs=256k count=1
+            dd if=$UPDATE_DTB_SOURCE of=/dev/dtb bs=256k
             ;;
           /dev/mmc*|LABEL=*)
             mount -o rw,remount $BOOT_ROOT
@@ -73,7 +62,6 @@ for arg in $(cat /proc/cmdline); do
       fi
       ;;
     disk=*)
-      echo "*** updating DISK partition label ..."
       disk="${arg#*=}"
       case $disk in
         /dev/mmc*)
@@ -86,21 +74,3 @@ for arg in $(cat /proc/cmdline); do
       ;;
   esac
 done
-
-if [ -f $SYSTEM_ROOT/usr/share/bootloader/boot.ini ]; then
-  echo "*** updating Odroid-C2 boot.ini ..."
-  mount -o rw,remount $BOOT_ROOT
-  cp -p $SYSTEM_ROOT/usr/share/bootloader/boot.ini $BOOT_ROOT/boot.ini.update
-fi
-
-if [ -f $SYSTEM_ROOT/usr/share/bootloader/boot-logo.bmp.gz ]; then
-  echo "*** updating Odroid-C2 boot logo ..."
-  mount -o rw,remount $BOOT_ROOT
-  cp -p $SYSTEM_ROOT/usr/share/bootloader/boot-logo.bmp.gz $BOOT_ROOT
-fi
-
-if [ -f $SYSTEM_ROOT/usr/share/bootloader/u-boot -a ! -e /dev/system -a ! -e /dev/boot ]; then
-  echo "*** updating u-boot for Odroid-C2 on: $BOOT_DISK ..."
-  dd if=$SYSTEM_ROOT/usr/share/bootloader/u-boot of=$BOOT_DISK conv=fsync bs=1 count=112 status=none
-  dd if=$SYSTEM_ROOT/usr/share/bootloader/u-boot of=$BOOT_DISK conv=fsync bs=512 skip=1 seek=1 status=none
-fi
