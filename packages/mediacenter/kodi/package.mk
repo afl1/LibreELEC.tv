6c04d0a7c80d999c8aea9afa0ef9f4883efefc4e
Adam Green <greena88@gmail.com>
2017-11-23 21:09:51 +0000

    kodi: update pkg

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="1ad3376"
+PKG_VERSION="cc10efe"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
